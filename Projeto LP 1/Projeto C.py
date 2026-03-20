23159c6c41ae09e58ffd1b1d93b3cd696ac910a2
Fernando Rabelo <fernandorabelol52@gmail.com>
2026-03-20 19:48:47 -0300

    porta

--- Projeto LP 1/Projeto C.py
+++ Projeto LP 1/Projeto C.py
@@ -1,15 +1,26 @@
-#inserindo os dados do produto a ser calculado para obter o lucro
-nome_produto = str(input("Digite o nome do produto: "))
-preço_produto = float(input("Digite o preço para produzir o produto: "))
-preço_venda = float(input("Digite o preço de venda do produto: "))
-quantidade_produzida = int(input("Digite a quantidade produzida do produto: "))
-quantidade_vendida = int(input("Digite a quantidade vendida do produto: "))
+while True:
+    #inserindo os dados do produto a ser calculado para obter o lucro
+    nome_produto = str(input("Digite o nome do produto (Digite 'sair' para finalizar): "))
+    preço_produto = float(input("Digite o preço para produzir o produto: "))
+    preço_venda = float(input("Digite o preço de venda do produto: "))
+    quantidade_produzida = int(input("Digite a quantidade produzida do produto: "))
+    quantidade_vendida = int(input("Digite a quantidade vendida do produto: "))
 
-#calculo do faturamento
-custo_produto = preço_produto * quantidade_produzida
-receita = preço_venda * quantidade_vendida
-lucro = receita - custo_produto
+    #calculo do faturamento
+    custo_produto = preço_produto * quantidade_produzida
+    receita = preço_venda * quantidade_vendida
+    lucro = receita - custo_produto
 
-print(f"O faturamento foi de R${receita:.2f}")
-print(f"O custo foi de R${custo_produto:.2f}")
-print(f"O lucro foi de R${lucro:.2f}")
+    #exibindo os resultados
+    print(f"O faturamento foi de R${receita:.2f}")
+    print(f"O custo foi de R${custo_produto:.2f}")
+    
+    #verificando se o produto deu lucro ou prejuízo
+    if receita > custo_produto:
+        print(f"O lucro foi de R${lucro:.2f}")
+    else:
+        print(f"O prejuízo foi de R${abs(lucro):.2f}")
+    
+    #condição de parada
+    if nome_produto == "sair":
+        break
